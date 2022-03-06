66d52aa7b25f9b37bdbe38d7785265f931ae3f2d
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2022-03-06 03:07:42 +0100

    no message

--- redpanda/entrypoint.sh
+++ redpanda/entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-while true; do
-  echo "RedPanda CLI.."
-  sleep 1
-done
+# NODE_ID=`shuf -i 1-100000 -n 1`
+MY_IP=`cat /etc/hosts  | grep "${HOSTNAME}" | awk '{print $1}'`
+
+if [[ "${NODE_TYPE}" = "root" ]]; then
+  rpk config bootstrap --id "${NODE_ID}" --self "${MY_IP}"
+  rpk redpanda start
+elif [[ "${NODE_TYPE}" = "node" ]]; then
+  ROOT_IP=`dig +short "${ROOT_NODE}"`
+  rpk config bootstrap --id "${NODE_ID}" --self "${MY_IP}" --ips "${ROOT_IP}"
+  rpk redpanda start
+fi
