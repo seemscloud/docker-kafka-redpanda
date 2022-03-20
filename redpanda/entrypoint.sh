b4ced144cab21c8d1804f762e6fe0ba313f22e41
mateusz.adam.katana@gmail.com <mateusz.adam.katana@gmail.com>
2022-03-21 00:03:58 +0100

    no message

--- redpanda/entrypoint.sh
+++ redpanda/entrypoint.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-# NODE_ID=`shuf -i 1-100000 -n 1`
-MY_IP=`cat /etc/hosts  | grep "${HOSTNAME}" | awk '{print $1}'`
+sleep 1
 
-if [[ "${NODE_TYPE}" = "root" ]]; then
-  rpk config bootstrap --id "${NODE_ID}" --self "${MY_IP}"
-  rpk redpanda start
-elif [[ "${NODE_TYPE}" = "node" ]]; then
-  ROOT_IP=`dig +short "${ROOT_NODE}"`
-  rpk config bootstrap --id "${NODE_ID}" --self "${MY_IP}" --ips "${ROOT_IP}"
-  rpk redpanda start
-fi
+export NODE_IP=$(cat /etc/hosts | grep "${HOSTNAME}" | awk '{print $1}')
+
+(
+  echo "cat << EndOfMessage"
+  cat redpanda.yaml.tmp
+  echo -e "\nEndOfMessage"
+) | bash >redpanda.yaml
+
+rpk redpanda start \
+  --config redpanda.yaml --check=false --overprovisioned \
+  --smp 1 --memory 1G --reserve-memory 0M \
+  --default-log-level=info
